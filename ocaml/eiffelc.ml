254120a179ccde6dafe6d8495e5b1d2ccc07d36f
Helmut Brandl <helmut.brandl@gmx.net>
2014-02-24 07:30:32 -0600

    Clean up of 'eiffelc.ml'
    The parsing of arguments is now done in a separate function and not
    in the main function of 'eiffelc.ml'.
    
    	modified:   ocaml/eiffelc.ml

--- ocaml/eiffelc.ml
+++ ocaml/eiffelc.ml
@@ -1,68 +1,15 @@
-open Support
 open Container
+open Support
+open Context
 
 let usage_string = "\
 Usage:
     eiffelc options srcfile.e
 "
 
+let file_name = ref ""
 
-let parse_file fn =
-  let lexbuf = Lexing.from_channel (open_in fn)
-  in
-  lexbuf.Lexing.lex_curr_p <-
-    {lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = fn};
-  Support.parse_error_fun := Lexer.print_unexpected;
-  Parser.main Lexer.token lexbuf
-
-
-
-
-
-module Analyze = struct
-
-  let analyze(ast:declaration list) =
-    let block_stack = Block_stack.make ()
-    and class_table = Class_table.base_table ()
-    and feature_table = Feature_table.empty ()
-    and ass_table = Assertion_table.empty ()
-    in
-    let rec analyz (ast: declaration list) =
-      let one_decl (d:declaration) =
-        match d with
-          Class_declaration (hm, cname, fgens, inherits, decl_blocks) ->
-            assert (fgens.v = []);
-            assert (inherits = []);
-            assert (decl_blocks = []);
-            Class_table.put hm cname class_table
-        | Declaration_block (Feature_block (visi,dlist)) ->
-            Block_stack.push visi block_stack;
-            analyz dlist;
-            Block_stack.pop block_stack;
-            ()
-        | Named_feature (fn, entlst, rt, body) ->
-            Feature_table.put fn entlst rt body
-              block_stack class_table feature_table
-        | Assertion_feature (label, entlst, body) ->
-            Prover.prove_and_store
-              entlst body class_table feature_table ass_table
-        | Formal_generic (name, concept) ->
-            Class_table.put_formal name concept class_table
-        | _ ->
-            Class_table.print   class_table;
-            Feature_table.print class_table feature_table;
-            assert false
-      in
-      match ast with
-        [] -> ()
-      | f::t -> one_decl f; analyz t
-    in
-    analyz ast
-end
-
-
-let _ =
-  let file_name = ref "" in
+let parse_arguments (): unit =
   let anon_fun str =
     if !file_name <> "" then
       raise (Arg.Bad "Only one source file allowed")
@@ -85,33 +32,95 @@ let _ =
     else
       raise (Arg.Bad "")
   in
+  Arg.parse
+    [("-trace",  Arg.String set_tracer,"{proof,failed-proof}");
+     ("-prover", Arg.String set_prover, "{basic,local}");
+     ("-statistics", Arg.Unit Options.set_statistics, "");
+     ("-goal-limit",
+      Arg.Int
+        (fun i ->
+          if i<0 then
+            raise (Arg.Bad "")
+          else
+            Options.set_goal_limit i),
+      "maximum number of goals per proof");
+     ("-no-limit", Arg.Unit Options.set_no_limit, "")
+   ]
+    anon_fun
+    usage_string;
+  if !file_name = "" then
+    raise (Support.Eiffel_error ("Need a source file\n" ^ usage_string))
+
+
+let parse_file fn =
+  let lexbuf = Lexing.from_channel (open_in fn)
+  in
+  lexbuf.Lexing.lex_curr_p <-
+    {lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = fn};
+  Support.parse_error_fun := Lexer.print_unexpected;
+  Parser.main Lexer.token lexbuf
+
+
+
+
+
+let analyze(ast:declaration list): unit =
+  let block_stack = Block_stack.make ()
+  and class_table = Class_table.base_table ()
+  and feature_table = Feature_table.empty ()
+  and ass_table = Assertion_table.empty ()
+  and context = Context.make ()
+  in
+  let rec analyz (ast: declaration list): unit =
+    let one_decl (d:declaration) =
+      match d with
+        Class_declaration (hm, cname, fgens, inherits, decl_blocks) ->
+          assert (fgens.v = []);
+          assert (inherits = []);
+          assert (decl_blocks = []);
+          Class_table.put hm cname class_table
+      | Declaration_block (Feature_block (visi,dlist)) ->
+          Block_stack.push visi block_stack;
+          Context.set_visibility visi context;
+          analyz dlist;
+          Context.reset_visibility context;
+          Block_stack.pop block_stack
+      | Named_feature (fn, entlst, rt, body) ->
+            Context.put_feature fn entlst rt body context;
+          Feature_table.put fn entlst rt body
+            (Block_stack.is_private block_stack)
+            class_table feature_table
+      | Assertion_feature (label, entlst, body) ->
+          Prover.prove_and_store
+            entlst body class_table feature_table ass_table
+      | Formal_generic (name, concept) ->
+          Class_table.put_formal name concept class_table;
+          Context.put_formal_generic name concept context
+      | _ ->
+          Class_table.print   class_table;
+          Feature_table.print class_table feature_table;
+          assert false
+    in
+    match ast with
+      [] -> ()
+      | f::t -> one_decl f; analyz t
+  in
+  analyz ast
+
+
+
+let _ =
   try
-    Arg.parse
-      [("-trace",Arg.String set_tracer,"{proof,failed-proof}");
-       ("-prover", Arg.String set_prover, "{basic,local}");
-       ("-statistics", Arg.Unit Options.set_statistics, "");
-       ("-goal-limit",
-        Arg.Int
-          (fun i ->
-            if i<0 then
-              raise (Arg.Bad "")
-            else
-              Options.set_goal_limit i),
-        "maximum number of goals per proof");
-       ("-no-limit", Arg.Unit Options.set_no_limit, "")
-     ]
-      anon_fun
-      usage_string;
-    if !file_name = "" then
-      raise (Support.Eiffel_error ("Need a source file\n" ^ usage_string));
+    parse_arguments ();
     try
-      Analyze.analyze (parse_file !file_name);
+      let ast = parse_file !file_name in
+      analyze ast;
       Statistics.write ();
     with Support.Error_info (inf,str) ->
       raise (Support.Error_fileinfo (!file_name,inf,str))
   with
     Support.Eiffel_error str
-  | Sys_error str ->  prerr_string str; exit 1
+  | Sys_error str ->  prerr_string (str ^ "\n"); exit 1
   | Support.Error_fileinfo (fn,inf,str) ->
-      prerr_string ((Support.info_string fn inf) ^ " " ^ str); exit 1
+      prerr_string ((Support.info_string fn inf) ^ " " ^ str ^ "\n"); exit 1
   | Parsing.Parse_error -> exit 1
