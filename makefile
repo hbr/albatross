ea4b4d792813e812d0a4763fe50db4adf04b03b9
Helmut Brandl <helmut.brandl@gmx.net>
2017-01-12 13:10:22 -0600

    Added files 'opam', 'alba.install' and 'makefile' to prepare installation via opam
    Furthermore the module 'command_line' has been added which is not yet used.
    
    	modified:   .gitignore
    	new file:   alba.install
            new file:   makefile
    	new file:   ocaml/command_line.ml
    	new file:   ocaml/command_line.mli
    	new file:   ocaml/makefile
    	new file:   opam

---
+++ makefile
@@ -0,0 +1,7 @@
+.PHONY: build
+
+build:
+	cd ocaml;       \
+	make alba;      \
+	make alba-debug
+
---
+++ ocaml/command_line.mli
@@ -0,0 +1,17 @@
+(* Copyright (C) 2017 Helmut Brandl  <helmut dot brandl at gmx dot net>
+
+   This file is distributed under the terms of the GNU General Public License
+   version 2 (GPLv2) as published by the Free Software Foundation.
+*)
+
+type t
+
+val command: t -> string list
+
+val working_directory: t -> string
+
+val verbosity: t -> int
+
+val arguments: t -> string list
+
+val get: string -> t
