afb8fe86588adbaf86a4d8df826afc0e6bb0285a
hbr <helmut.brandl@gmx.net>
2018-03-23 09:34:54 -0600

    Second draft of 'Incremental_parser'
    	modified:   ocaml/lib/incremental_parser.ml

--- ocaml/lib/incremental_parser.ml
+++ ocaml/lib/incremental_parser.ml
@@ -15,6 +15,9 @@ module type PARSER =
     val next:    'a t -> token -> 'a t
     val consume: 'a t -> token list -> 'a t
 
+    module M: Monad.STATE_WITH_RESULT with type state = state and
+                                           type error = error
+
     type ('a,'z) context =
       {state:state;
        success: state -> 'a -> token list -> 'z t;
@@ -26,18 +29,43 @@ module type PARSER =
     val consume: 'a t -> token list -> 'a t
     val next:    'a t -> token -> 'a t
 
-    val backtrack: ('a,'a) partial -> ('a,'z) context -> 'z t
+    val backtrack: ('a,'a) partial -> ('a,'z) partial
 
-    val token: (state -> token -> state * ('a,error) result) ->
-               ('a,'z) partial
+    val token: (token -> 'a M.t) -> ('a,'z) partial
 
-    val join: ('a -> 'b -> state -> state * 'c) ->
+    val join: (('a*'b) -> 'c M.t) ->
               ('a,'z) partial ->
               ('b,'z) partial ->
               ('c,'z) partial
 
-    val choose: ('a,'z) partial -> ('a,'z) partial -> ('a,'z) partial
-    val choose_l: ('a,'z) partial list -> ('a,'z) partial
+    val choose: (('a,'z) partial list) -> ('a,'z) partial
+
+    val choose2: ('a,'z) partial -> ('a,'z) partial -> ('a,'z) partial
+
+    val many_base: ('a,'z) partial ->
+                   (int -> bool) ->
+                   (int -> bool) ->
+                   'b ->
+                   ('b -> 'a -> 'b M.t) ->
+                   ('b -> 'c M.t) ->
+                   ('c,'z) partial
+
+    val many0: ('a,'z) partial -> ('a list -> 'c M.t) -> ('c,'z) partial
+
+    val many1: ('a,'z) partial -> ('a list -> 'c M.t) -> ('c,'z) partial
+
+    val between: ('a -> 'b -> 'c -> 'r M.t) ->
+                 ('a,'z) partial ->
+                 ('b,'z) partial ->
+                 ('c,'z) partial ->
+                 ('r,'z) partial
+
+    val count: int -> int ->
+               ('a,'z) partial ->
+               ('a list -> 'b M.t) ->
+               ('b,'z) partial
+
+    val optional: ('a,'z) partial -> ('a option,'z) partial
   end
 
 
@@ -90,33 +118,19 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
       consume p [t]
 
 
-    module ST = State_transformer.Make (State)
-    module M =
-      struct
-        type 'a pt = 'a t
-        include Result.Within
-                  (ST)
-                  (Error)
-        let run (s:state) (m:'a t) (f1:state->'a->'b) (f2:state->'e->'b): 'b =
-          let r,s = ST.run s m in
-          match r with
-          | Ok a ->
-             f1 s a
-          | Error e ->
-             f2 s e
-      end
+    module M = Monad.State_with_result (State) (Error)
 
 
-    let one (f:token -> 'a M.t) (c:('a,'z) context): 'z t =
+    let token (f:token -> 'a M.t) (c:('a,'z) context): 'z t =
       More (c.state,
             M.(fun s t ->
-              run
+              continue
                 s (f t)
                 (fun s a -> c.success s a [])
                 (fun s e -> c.failure s e 0 [t])
             ))
 
-    let join_new
+    let join
           (f: ('a*'b) -> 'r M.t)
           (pp: ('a,'z) partial)
           (qq: ('b,'z) partial)
@@ -126,7 +140,7 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
         {state = s;
          success =
            (fun s b la ->
-             M.(run
+             M.(continue
                   s (f (a,b))
                   (fun s r -> c.success s r la)
                   (fun s e -> c.failure s e 0 la)));
@@ -139,6 +153,7 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
          failure = c.failure}
       in
       pp cp
+
 
     let backtrack
           (pp: ('a,'a) partial)
@@ -156,43 +171,7 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
       f (make pp c.state) []
 
 
-    let token
-          (f:state -> token -> state * ('a,error) result)
-          (k:('a,'z) context)
-        : 'z t =
-      More (k.state,
-            (fun s t ->
-              let s,r = f s t in
-              match r with
-              | Ok a ->
-                 k.success s a []
-              | Error e ->
-                 k.failure s e 0 [t])
-        )
-
-    let join
-          (f: 'a -> 'b -> state -> state * 'r)
-          (pp: ('a,'z) partial)
-          (qq: ('b,'z) partial)
-          (c: ('r,'z) context)
-        : 'z t =
-      let cq s a =
-        {state = s;
-         success =
-           (fun s b la ->
-             let s,r = f a b s in
-             c.success s r la);
-         failure = c.failure}
-      in
-      let cp =
-        {state = c.state;
-         success =
-           (fun s a la -> List.fold_left next (cq s a |> qq) (List.rev la));
-         failure = c.failure}
-      in
-      pp cp
-
-    let choose_l (l: ('a,'z) partial list) (c: ('a,'z) context): 'z t =
+    let choose (l: ('a,'z) partial list) (c: ('a,'z) context): 'z t =
       match l with
       | [] ->
          assert false (* illegal call without alternatives *)
@@ -216,19 +195,99 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
          pp (context c.state rest)
 
 
-    let choose
+    let choose2 (pp: ('a,'z) partial) (qq: ('a,'z) partial): ('a,'z) partial =
+      choose [pp;qq]
+
+
+    let many_base
           (pp: ('a,'z) partial)
-          (qq: ('a,'z) partial)
-          (c: ('a,'z) context)
+          (sufficient: int -> bool)
+          (flush: int -> bool)
+          (b0:'b)
+          (f1: 'b -> 'a -> 'b M.t)
+          (f2: 'b -> 'c M.t)
+          (c:('c,'z) context)
         : 'z t =
-      pp {state = c.state;
-          success = c.success;
-          failure =
-            (fun s e n la ->
-              if n = 0 then
-                qq {state = s;
-                    success = c.success;
-                    failure = c.failure}
-              else
-                c.failure s e n la)}
+      let rec many (i:int) (b:'b) (s:state): 'z t =
+        let continue s b i la =
+          M.(continue
+               s (f2 b)
+               (fun s r -> c.success s r la)
+               (fun s e -> c.failure s e 0 la))
+        in
+        if sufficient i && flush i then
+          continue s b i []
+        else
+          pp {state = s;
+              success =
+                (fun s a la ->
+                  M.continue
+                    s (f1 b a)
+                    (fun s b -> List.fold_left next (many (i+1) b s) (List.rev la))
+                    (fun s e -> c.failure s e 0 la));
+              failure =
+                (fun s e n la ->
+                  if n = 0 && sufficient i then
+                    continue s b i la
+                  else
+                    c.failure s e n la)}
+      in
+      many 0 b0 c.state
+
+
+    let many0
+          (pp: ('a,'z) partial)
+          (f:'a list -> 'c M.t)
+        : ('c,'z) partial =
+      many_base
+        pp (fun _ -> true) (fun _ -> false)
+        [] (fun l a -> a :: l |> M.make) f
+
+    let many1
+          (pp: ('a,'z) partial)
+          (f:'a list -> 'c M.t)
+        : ('c,'z) partial =
+      many_base
+        pp
+        (fun i -> i <> 0)
+        (fun _ -> false)
+        [] (fun l a -> a :: l |> M.make) f
+
+
+    let count
+          (i0:int) (i1:int)
+          (pp: ('a,'z) partial)
+          (f:'a list -> 'c M.t)
+        : ('c,'z) partial =
+      assert (0 <= i0);
+      assert (i0 <= i1);
+      many_base
+        pp (fun i -> i0 <= i) (fun i -> i = i1)
+        [] (fun l a -> a :: l |> M.make) f
+
+
+
+    let optional (pp:('a,'z) partial): ('a option,'z) partial =
+      count 0 1 pp
+        (fun l ->
+          match l with
+          | [] ->
+             None |> M.make
+          | [x] ->
+             Some x |> M.make
+          | _ ->
+             assert false)
+
+
+
+    let between
+          (f: 'a -> 'b -> 'c -> 'r M.t)
+          (pp1: ('a,'z) partial)
+          (pp2: ('b,'z) partial)
+          (pp3: ('c,'z) partial)
+        : ('r,'z) partial =
+      join
+        (fun ((a,b),c) -> f a b c)
+        (join M.make pp1 pp2)
+        pp3
   end
