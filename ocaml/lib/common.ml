f8b9d233d17a79ce69629fa310c7d961816fbeab
hbr <helmut.brandl@gmx.net>
2018-03-24 10:57:32 -0600

    Added module 'Char_' and 'String_' to 'common.ml'
    	modified:   ocaml/lib/common.ml
    	modified:   ocaml/lib/common.mli

--- ocaml/lib/common.ml
+++ ocaml/lib/common.ml
@@ -12,3 +12,48 @@ module Either =
     let left a = Left a
     let right b = Right b
   end
+
+
+module Char_ =
+  struct
+    include Char
+    let is_lower (c:char): bool =
+      'a' <= c && c <= 'z'
+    let is_upper (c:char): bool =
+      'A' <= c && c <= 'Z'
+    let is_letter (c:char): bool =
+      is_lower c || is_upper c
+    let is_digit (c:char): bool =
+      '0' <= c && c <= '9'
+  end
+
+module String_ =
+  struct
+    include String
+
+    let one (c:char): string =
+      String.make 1 c
+
+    let list (s:string): char list =
+      let rec list cs i =
+        if i = 0 then
+          cs
+        else
+          let j = i - 1 in
+          list (s.[j]::cs) j
+      in
+      list [] (length s)
+
+    let of_list (cs:char list): string =
+      let rec str cs i =
+        match cs with
+        | [] ->
+           Bytes.create i
+        | c::cs ->
+           let bs = str cs (i+1) in
+         Bytes.set bs i c;
+         bs
+      in
+      let bs = str cs 0 in
+      Bytes.unsafe_to_string bs
+  end
--- ocaml/lib/common.mli
+++ ocaml/lib/common.mli
@@ -12,3 +12,25 @@ sig
   val left:  'a -> ('a,'b) t
   val right: 'b -> ('a,'b) t
 end
+
+
+module Char_:
+sig
+  type t = char
+  val code: t -> int
+  val chr:  int -> t
+  val compare: t -> t -> int
+  val escaped: t -> string
+  val is_lower: t -> bool
+  val is_upper: t -> bool
+  val is_letter: t -> bool
+  val is_digit:  t -> bool
+end
+
+module String_:
+sig
+  type t = string
+  val one: char -> t
+  val list: t -> char list
+  val of_list: char list -> t
+end
