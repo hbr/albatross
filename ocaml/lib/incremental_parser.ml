3b3e0f05fe25cb779dd2e4b347a457fe1e444ac2
hbr <helmut.brandl@gmx.net>
2018-03-24 12:55:06 -0600

    Added the number of consumed tokens to the incremental parser
    	modified:   ocaml/lib/incremental_parser.ml

--- ocaml/lib/incremental_parser.ml
+++ ocaml/lib/incremental_parser.ml
@@ -7,7 +7,7 @@ module type PARSER =
     type error
     type state
     type 'a t =
-      | Accept of state * 'a * token list
+      | Accept of state * 'a * int * token list
       | More of state * (state -> token -> 'a t)
       | Reject of state * error * int * token list
 
@@ -19,7 +19,7 @@ module type PARSER =
 
     type ('a,'z) context =
       {state:state;
-       success: state -> 'a -> token list -> 'z t;
+       success: state -> 'a -> int -> token list -> 'z t;
        failure: state -> error -> int -> token list -> 'z t}
 
     type ('a,'z) partial = ('a,'z) context -> 'z t
@@ -55,11 +55,13 @@ module type PARSER =
 
     val many1: ('a,'z) partial -> ('a list,'z) partial
 
-    val between: ('a -> 'b -> 'c -> 'r M.t) ->
-                 ('a,'z) partial ->
+    val many0_separated: ('a,'z) partial -> ('b,'z) partial -> ('a list,'z) partial
+    val many1_separated: ('a,'z) partial -> ('b,'z) partial -> ('a list,'z) partial
+
+    val between: ('a,'z) partial ->
                  ('b,'z) partial ->
                  ('c,'z) partial ->
-                 ('r,'z) partial
+                 ('b,'z) partial
 
     val count: int -> int ->
                ('a,'z) partial ->
@@ -81,7 +83,7 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
     type error = Error.t
     type state = State.t
     type 'a t =
-      | Accept of state * 'a * token list
+      | Accept of state * 'a * int * token list
       | More of state * (state -> token -> 'a t)
       | Reject of state * error * int * token list
 
@@ -89,7 +91,7 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
 
     type ('a,'z) context =
       {state:state;
-       success: state -> 'a -> tlist -> 'z t;
+       success: state -> 'a -> int -> tlist -> 'z t;
        failure: state -> error -> int -> tlist -> 'z t}
 
     type ('a,'z) partial =
@@ -98,13 +100,13 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
 
     let make (pp:('a,'a) partial) (state:state): 'a t =
       pp {state;
-          success = (fun s a la -> Accept (s,a,la));
+          success = (fun s a n la -> Accept (s,a,n,la));
           failure = (fun s e n la -> Reject (s,e,n,la))}
 
     let rec consume (p:'a t) (ts:tlist): 'a t =
       match p with
-      | Accept (s, a, la) ->
-         Accept (s, a, List.rev_append ts la)
+      | Accept (s, a, n, la) ->
+         Accept (s, a, n, List.rev_append ts la)
       | More (s, f) ->
          begin
            match ts with
@@ -122,12 +124,15 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
     module M = Monad.State_with_result (State) (Error)
 
 
+    let succeed (a:'a) (c:('a,'z) context): 'z t =
+      c.success c.state a 0 []
+
     let token (f:token -> 'a M.t) (c:('a,'z) context): 'z t =
       More (c.state,
             M.(fun s t ->
               continue
                 s (f t)
-                (fun s a -> c.success s a [])
+                (fun s a -> c.success s a 1 [])
                 (fun s e -> c.failure s e 0 [t])
             ))
 
@@ -137,20 +142,21 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
           (f: ('a*'b) -> 'r M.t)
           (c: ('r,'z) context)
         : 'z t =
-      let cq s a =
+      let cq s a n0 =
         {state = s;
          success =
-           (fun s b la ->
+           (fun s b n la ->
              M.(continue
                   s (f (a,b))
-                  (fun s r -> c.success s r la)
-                  (fun s e -> c.failure s e 0 la)));
-         failure = c.failure}
+                  (fun s r -> c.success s r (n+n0) la)
+                  (fun s e -> c.failure s e (n+n0) la)));
+         failure =
+           (fun s e n la -> c.failure s e (n+n0) la)}
       in
       let cp =
         {state = c.state;
          success =
-           (fun s a la -> List.fold_left next (cq s a |> qq) (List.rev la));
+           (fun s a n la -> List.fold_left next (cq s a n |> qq) (List.rev la));
          failure = c.failure}
       in
       pp cp
@@ -170,8 +176,8 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
       let s0 = c.state in
       let rec f (p:'a t) (used:tlist): 'z t =
         match p with
-        | Accept (s,a,la) ->
-           c.success s a la
+        | Accept (s,a,n,la) ->
+           c.success s a n la
         | More (s,fp) ->
            More (s, fun s t -> f (fp s t) (t::used))
         | Reject (s,e,n,la) ->
@@ -217,31 +223,57 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
           (f2: 'b -> 'c M.t)
           (c:('c,'z) context)
         : 'z t =
-      let rec many (i:int) (b:'b) (s:state): 'z t =
-        let continue s b i la =
+      let rec many (i:int) (ntoks:int) (b:'b) (s:state): 'z t =
+        let continue s b ntoks (la:tlist) =
           M.(continue
                s (f2 b)
-               (fun s r -> c.success s r la)
-               (fun s e -> c.failure s e 0 la))
+               (fun s r -> c.success s r ntoks la)
+               (fun s e -> c.failure s e ntoks la))
         in
         if sufficient i && flush i then
-          continue s b i []
+          continue s b ntoks []
         else
           pp {state = s;
               success =
-                (fun s a la ->
+                (fun s a n la ->
                   M.continue
                     s (f1 b a)
-                    (fun s b -> List.fold_left next (many (i+1) b s) (List.rev la))
-                    (fun s e -> c.failure s e 0 la));
+                    (fun s b ->
+                      List.fold_left next (many (i+1) (ntoks+n) b s) (List.rev la))
+                    (fun s e -> c.failure s e n la));
               failure =
                 (fun s e n la ->
                   if n = 0 && sufficient i then
-                    continue s b i la
+                    continue s b ntoks la
                   else
-                    c.failure s e n la)}
+                    c.failure s e (ntoks+n) la)}
       in
-      many 0 b0 c.state
+      many 0 0 b0 c.state
+
+
+    let count
+          (i0:int) (i1:int)
+          (pp: ('a,'z) partial)
+          (f:'a list -> 'c M.t)
+        : ('c,'z) partial =
+      assert (0 <= i0);
+      assert (i0 <= i1);
+      many_base
+        pp (fun i -> i0 <= i) (fun i -> i = i1)
+        [] (fun l a -> a :: l |> M.make) f
+
+
+    let optional (pp:('a,'z) partial): ('a option,'z) partial =
+      count 0 1 pp
+        (fun l ->
+          match l with
+          | [] ->
+             None |> M.make
+          | [x] ->
+             Some x |> M.make
+          | _ ->
+             assert false (* not more than one element is in the list*)
+        )
 
 
     let many0
@@ -260,43 +292,32 @@ module Make (Token:ANY) (Error:ANY) (State:ANY)
         (fun _ -> false)
         [] (fun l a -> a :: l |> M.make) (fun l -> List.rev l |> M.make)
 
+    let many1_separated
+          (pp:('a,'z) partial)
+          (sep: ('b,'z) partial)
+        : ('a list,'z) partial =
+      join_base
+        pp
+        (many0 (join_base sep pp (fun (_,a) -> M.make a)))
+        (fun (a,l) -> a :: l |> M.make)
 
-    let count
-          (i0:int) (i1:int)
-          (pp: ('a,'z) partial)
-          (f:'a list -> 'c M.t)
-        : ('c,'z) partial =
-      assert (0 <= i0);
-      assert (i0 <= i1);
-      many_base
-        pp (fun i -> i0 <= i) (fun i -> i = i1)
-        [] (fun l a -> a :: l |> M.make) f
-
-
-
-    let optional (pp:('a,'z) partial): ('a option,'z) partial =
-      count 0 1 pp
-        (fun l ->
-          match l with
-          | [] ->
-             None |> M.make
-          | [x] ->
-             Some x |> M.make
-          | _ ->
-             assert false)
-
-
+    let many0_separated
+          (pp:('a,'z) partial)
+          (sep: ('b,'z) partial)
+        : ('a list,'z) partial =
+      choose2
+        (many1_separated pp sep)
+        (succeed [])
 
     let between
-          (f: 'a -> 'b -> 'c -> 'r M.t)
           (pp1: ('a,'z) partial)
           (pp2: ('b,'z) partial)
           (pp3: ('c,'z) partial)
-        : ('r,'z) partial =
+        : ('b,'z) partial =
       join_base
         (join pp1 pp2)
         pp3
-        (fun ((a,b),c) -> f a b c)
+        (fun ((a,b),c) -> M.make b)
   end
 
 
@@ -377,7 +398,7 @@ module Character_parser =
 let test (): unit =
   let open Printf in
   let open Character_parser in
-  let print_result (p:'a t) (f:'a -> string): unit =
+  let print_result (str:string) (p:'a t) (f:'a -> string): unit =
     let rest la =
       let rec rest0 l la =
         match la with
@@ -390,34 +411,34 @@ let test (): unit =
       in
       String_.of_list (rest0 [] la)
     in
+    printf "input '%s' " str;
     match p with
-    | Accept (s,a,la) ->
-       printf "(%d,%d) found '%s', rest '%s'\n"
+    | Accept (s,a,n,la) ->
+       printf "(%d,%d) found '%s', consumed %d, rest '%s'\n"
          s.State.line s.State.column
          (f a)
-         (rest la)
+         n (rest la)
     | More (s,g) ->
        printf "(%d,%d) not yet complete\n" s.State.line s.State.column
     | Reject (s,e,n,la) ->
-       printf "(%d,%d) error '%s', consumed %d, rest '%s'\n"
+       printf "(%d,%d) error %s, consumed %d, rest '%s'\n"
          s.State.line s.State.column
          e n (rest la)
   in
   let run (p:'a t) (str:string) (f:'a -> string): unit =
-    print_result (run_string str p) f
+    print_result str (run_string str p) f
   in
   printf "Test character parser\n";
   (*run
     (choose2 (many1 letter M.make) (many1 digit M.make) |> make)
     "1234"
     (fun l -> List.rev l |> String_.of_list);*)
-  (*run
-    (join (many1 letter) (many1 digit) |> make)
-    "hello1234:"
-    (fun (a,b) ->
-      String_.of_list a ^ ","
-      ^ String_.of_list b)*)
   run
+    (many1_separated (choose2 (many1 letter) (many1 digit)) (expect ' ') |> make)
+    "hello 1 2 3 4 r:"
+    (fun l ->
+      String.concat " " (List.map String_.of_list l))
+  (*run
     ((join letter letter) |> make)
     "h.ello."
-    (fun (a,b) -> String_.one a ^ String_.one b)
+    (fun (a,b) -> String_.one a ^ String_.one b)*)
