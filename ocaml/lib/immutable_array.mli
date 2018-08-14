8f58bcd87336fb660f515768de00a32fa10334ec
hbr <helmut.brandl@gmx.net>
2018-08-14 15:51:59 -0500

    Interface of Immutable_array to list added
    	modified:   ocaml/lib/immutable_array.ml
    	modified:   ocaml/lib/immutable_array.mli

--- ocaml/lib/immutable_array.mli
+++ ocaml/lib/immutable_array.mli
@@ -5,4 +5,6 @@ val length: 'a t -> int
 val elem:   int -> 'a t -> 'a
 val put:    int -> 'a -> 'a t -> 'a t
 val push:   'a -> 'a t -> 'a t
+val push_list: 'a list -> 'a t -> 'a t
+val of_list:   'a list -> 'a t
 val take:   int -> 'a t -> 'a t
