d6792015fcb31c86bc70a455fed6a1dd9b183783
hbr <helmut.brandl@gmx.net>
2019-04-27 13:18:18 -0500

    Added fmlib.opam and lib.mli
    fmlib.opam is needed to treat the library as an own opam project
    
    lib.mli makes only some module of the basic library visible to
    the users of the library.
    
    	new file:   fmlib.opam
    	modified:   ocaml/lib/dune
    	new file:   ocaml/lib/lib.mli

---
+++ ocaml/lib/lib.mli
@@ -0,0 +1,17 @@
+module Common_module_types  = Common_module_types
+(** Common module types like [ANY, SORTABLE, ...] *)
+
+module Common = Common
+
+module Option = Option
+module Monad  = Monad
+
+module Io = Io
+module Ocaml_io = Ocaml_io
+
+module Document     = Document
+module Basic_parser = Basic_parser
+
+module Pretty_printer  = Pretty_printer
+module Pretty_printer2 = Pretty_printer2
+module Argument_parser = Argument_parser
