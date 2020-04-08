912250062f8d58b93b30d6a9d3411d6ecfa34007
hbr <helmut.brandl@gmx.net>
2020-04-07 20:13:33 -0500

    Webapp: Simplified 'Alba_web2'
    One generic module removed.
    
    	modified:   ocaml/alba-web/alba_web2.ml

--- ocaml/alba-web/alba_web2.ml
+++ ocaml/alba-web/alba_web2.ml
@@ -1,39 +1,37 @@
 open Fmlib
 open Common
 
-module Alba_web (Browser: Html2.BROWSER) =
-struct
-    module Vdom = Html2.Vdom (Browser)
+
+module Browser = Fmlib_js.Browser
 
 
-    let model: string =
-        ""
+module Vdom = Html2.Vdom (Browser)
 
-    let view (model: string): string Vdom.t =
-        let open Vdom in
-        let open Attribute in
-        div
-            []
-            [input
-                [   attribute "placeholder" "Text to reverse"
-                ;   property "value" model
-                ;   on "input" Browser.Decoder.string
-                ]
-                []
-            ;div [] [text (String.reverse model)]
+
+let model: string =
+    ""
+
+
+let view (model: string): string Vdom.t =
+    let open Vdom in
+    let open Attribute in
+    div
+        []
+        [input
+            [   attribute "placeholder" "Text to reverse"
+            ;   property "value" model
+            ;   on "input" Browser.Decoder.string
             ]
+            []
+        ;div [] [text (String.reverse model)]
+        ]
 
-    let update (message: string) (_: string): string =
-        message
 
-
-    let run (): unit =
-        let module Browser = Browser.Make (Vdom) in
-        Browser.sandbox model view update
-end
+let update (message: string) (_: string): string =
+    message
 
 
 
 let _ =
-    let module Browser = Alba_web (Fmlib_js.Browser) in
-    Browser.run ()
+    let module Browser = Browser.Make (Vdom) in
+    Browser.sandbox model view update
