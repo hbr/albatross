51d3772ef7ac4b839ef38224ab9fbe40b0c91288
hbr <helmut.brandl@gmx.net>
2020-04-24 09:56:23 -0500

    Restructured 'Name_map.add'
    	modified:   ocaml/core/name_map.ml

--- ocaml/core/name_map.ml
+++ ocaml/core/name_map.ml
@@ -38,26 +38,32 @@ let add (name:string) (global:bool) (m:t): t =
         {
             map =
                 String_map.add
-                 name
-                 (match String_map.maybe_find name m.map with
-                  | None ->
-                     [m.cnt]
-                  | Some lst ->
-                     if global then
-                       m.cnt :: lst
-                     else
-                       [m.cnt])
-                 m.map;
+                    name
+                    (
+                        if not global then
+                            [ m.cnt ]
+                        else
+                            match String_map.maybe_find name m.map with
+                            | None ->
+                                [ m.cnt ]
+                            | Some lst ->
+                                m.cnt :: lst
+                    )
+                    m.map;
+
             has_locs =
                 not global;
+
             cnt =
                 1 + m.cnt
         }
+
 
 let add_global (name:string) (m:t): t =
     assert (name <> "");
     assert (not (has_locals m));
     add name true m
+
 
 let add_local (name: string) (m:t) : t =
     assert (name <> "");
