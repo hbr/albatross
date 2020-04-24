37d9f06c9d280e3063c2f15a0a60fddb88b1c891
hbr <helmut.brandl@gmx.net>
2020-04-24 10:26:36 -0500

    Reformatted indentation
    	modified:   ocaml/core/context.ml

--- ocaml/core/context.ml
+++ ocaml/core/context.ml
@@ -10,42 +10,50 @@ type t = {
 
 
 
-let count (c:t): int =
-  Gamma.count c.gamma
+let count (c: t): int =
+    Gamma.count c.gamma
 
 
-let gamma (c:t): Gamma.t =
-  c.gamma
 
-let name_map (c:t): Name_map.t =
-  c.map
+let gamma (c: t): Gamma.t =
+    c.gamma
+
+
+
+let name_map (c: t): Name_map.t =
+    c.map
 
 
 let standard (): t =
-  let gamma = Gamma.standard () in
-  {gamma;
-   map =
-     Interval.fold
-       Name_map.empty
-       (fun i m ->
-         Name_map.add_global
-           Gamma.(name_at_level i gamma)
-           m)
-       0 (Gamma.count gamma)
-  }
+    let gamma = Gamma.standard () in
+    {gamma;
+     map =
+        Interval.fold
+            Name_map.empty
+            (fun i m ->
+                Name_map.add_global
+                    Gamma.(name_at_level i gamma)
+                    m
+            )
+            0
+            (Gamma.count gamma)
+    }
 
 
-let compute (t:Term.t) (c:t): Term.t =
-  Gamma.compute t c.gamma
+let compute (t: Term.t) (c: t): Term.t =
+    Gamma.compute t c.gamma
 
 
-let find_name (name:string) (c:t): int list =
-  Name_map.find name c.map
+let find_name (name: string) (c: t): int list =
+    Name_map.find name c.map
 
 
-module Pretty (P:Pretty_printer.SIG) =
-  struct
+
+
+module Pretty (P: Pretty_printer.SIG) =
+struct
     module P0 = Term_printer.Pretty (Gamma) (P)
+
     let print (t:Term.t) (c:t): P.t =
-      P0.print t c.gamma
-  end
+        P0.print t c.gamma
+end
