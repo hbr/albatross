9f78271cf0b7837bbdc43c747738f52f00349287
hbr <helmut.brandl@gmx.net>
2020-05-25 14:04:55 -0500

    Added unit tests for building inductive types
    	new file:   ocaml/albalib/test_inductive.ml

---
+++ ocaml/albalib/test_inductive.ml
@@ -0,0 +1,194 @@
+open Alba_core
+open Ast
+
+
+module Inductive_parser =
+    Parser_lang.Make (
+        struct
+            type t = Source_entry.inductive array
+        end)
+
+
+let add_inductive
+    (src: string)
+    (c: Context.t)
+    : (Context.t, Build_problem.t) result
+=
+    let open Inductive_parser in
+    let p = run (inductive_family ()) src
+    in
+    assert (has_ended p);
+    assert (has_succeeded p);
+    match result p with
+    | None ->
+        assert false
+    | Some inds ->
+        Builder.add_inductive inds c
+
+
+
+(*  Test the inductive types
+    ------------------------
+
+    - same number of parameters
+    - same parameter names
+    - same parameter types
+    - inductive type must be a type (i.e. its type a kind)
+*)
+
+
+let%test _ =
+    let src = "class I0 A :=\n\
+               class I1 A B :="
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Wrong_parameter_count _) ->
+        true
+    | _ ->
+        false
+
+
+let%test _ =
+    let src = "class I0 A :=\n\
+               class I1 B :="
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Wrong_parameter_name _) ->
+        true
+    | _ ->
+        false
+
+
+
+let%test _ =
+    let src = "class I0 (A: Any -> Any) :=\n\
+               class I1 A :="
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Wrong_parameter_type _) ->
+        true
+    | _ ->
+        false
+
+
+let%test _ =
+    let src = "class i A: Int :="
+    and context =
+        Context.(
+            empty
+            |>
+            add_builtin_type
+                "int_type"
+                "Int"
+                Term.any
+        )
+    in
+    match
+        add_inductive src context
+    with
+    | Error (_, Build_problem.No_inductive_type) ->
+        true
+    | _ ->
+        false
+
+
+
+(*  Test the constructors
+    ------------------------
+
+    - if there are indices, then type must be explicit
+    - construct a object of the corresponding inductive type
+    - positivity
+    - positivity in family
+    - positivity with other inductive type
+*)
+
+let%test _ =
+    let src = "class I A: A -> Any := constr"
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Missing_inductive_type) ->
+        true
+    | _ ->
+        false
+
+
+
+let%test _ =
+    let src = "class I A := Constr: Any"
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Wrong_type_constructed) ->
+        true
+    | _ ->
+        false
+
+
+
+let%test _ =
+    let src = "class I A := constr: I Proposition"
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Wrong_type_constructed) ->
+        true
+    | _ ->
+        false
+
+
+
+let%test _ =
+    let src = "class I := constr: (I -> Proposition) -> I"
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Negative) ->
+        true
+    | _ ->
+        false
+
+
+
+let%test _ =
+    let src = "class I A := constr: I (I A) -> I A"
+    in
+    match
+        add_inductive src Context.empty
+    with
+    | Error (_, Build_problem.Not_positive) ->
+        true
+    | _ ->
+        false
+
+
+(*
+let%test _ =
+    let src1 = "class I1 A := co1: (A -> A) -> I1 A"
+    and src2 = "class I := co : I1 I -> I"
+    in
+    match
+        add_inductive src1 Context.empty
+    with
+    | Error _ ->
+        false
+    | Ok c ->
+        match
+            add_inductive src2 c
+        with
+        | Error (_, Build_problem.Not_positive) ->
+            true
+        | _ ->
+            false
+*)
