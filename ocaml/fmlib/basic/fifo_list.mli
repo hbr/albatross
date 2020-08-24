f4b6fc62409c3104334ef0580821dde18e8a6542
hbr <helmut.brandl@gmx.net>
2020-08-24 11:09:53 -0500

    Fmlib: Added functions to 'Fifo_list'
    	modified:   ocaml/fmlib/basic/fifo_list.ml
    	modified:   ocaml/fmlib/basic/fifo_list.mli

--- ocaml/fmlib/basic/fifo_list.mli
+++ ocaml/fmlib/basic/fifo_list.mli
@@ -1,5 +1,9 @@
 type _ t
 
+val is_empty: _ t -> bool
+
+val has_some: _ t -> bool
+
 val empty: _ t
 
 val push_front: 'a -> 'a t -> 'a t
@@ -7,3 +11,9 @@ val push_front: 'a -> 'a t -> 'a t
 val push_rear: 'a -> 'a t -> 'a t
 
 val pop_front: 'a t -> ('a * 'a t) option
+
+val update_first: ('a -> 'a) -> 'a t -> 'a t
+
+val update_last: ('a -> 'a) -> 'a t -> 'a t
+
+val to_list: 'a t -> 'a list
